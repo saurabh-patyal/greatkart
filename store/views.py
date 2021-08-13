ff1d7cef3665de46e630a026c903df40b5d57bab
saurabh patyal <saurabh.patyal@gmail.com>
2021-08-13 14:22:21 +0530

    till pagination&search complate

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from django.db.models import Q
 # Create your views here.
 
 def store(request,category_slug=None):
@@ -43,3 +44,17 @@ def product_detail(request,category_slug,product_slug):
         'in_cart':in_cart,
     }
     return render(request,'store/product_detail.html',context)
+
+
+#============search functionality================
+def search(request):
+    if 'keyword' in request.GET:
+            keyword = request.GET['keyword']
+            if keyword:
+                products = Product.objects.order_by('-id').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+                product_count = products.count()
+                context = {
+                    'products': products,
+                    'product_count': product_count,
+                }
+            return render(request, 'store/store.html', context)
